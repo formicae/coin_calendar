31c82d2224b9e186e7ca19ce9be9e7ad09d04506
riceluxs1t <riceluxs1t@gmail.com>
2017-12-23 21:28:47 +0900

    apiserver side datascraper, predictionmodel, viewcomponent added.

--- apiserver/settings.py
+++ apiserver/settings.py
@@ -38,6 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'datascraper',
+    'prediction_model',
+    'view_component',
 ]
 
 MIDDLEWARE_CLASSES = [
@@ -120,3 +123,26 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+# Logging
+LOGGING_PATH = 'log/log_file'
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'file': {
+            'level': 'DEBUG',
+            'class': 'logging.FileHandler',
+            'filename': os.path.join(os.getcwd(), LOGGING_PATH)
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['file'],
+            'level': 'DEBUG',
+            'propagate': True,
+        },
+    },
+}
